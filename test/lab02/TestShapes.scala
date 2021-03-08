4dd0618348c7068fd8ddbf17bb75a8ecf81f173a
Simone Romagnoli <simone.romagnoli10@studio.unibo.it>
2021-03-08 17:24:26 +0100

    refactoring of shapes and shapes test

--- test/lab02/TestShapes.scala
+++ test/lab02/TestShapes.scala
@@ -8,25 +8,33 @@ class TestShapes {
 
   val height = 10.0
   val width = 20.0
+  val rectPerimeter = 60
+  val rectArea = 200
+
   val radius = 100.0
+  val circPerimeter = 628
+  val circArea = 31400
+
   val edge = 50.0
+  val squarePerimeter = 200
+  val squareArea = 2500
 
   @Test def testRectangle(): Unit = {
     val rect:Shape = Rectangle(height, width)
-    assertEquals(60, perimeter(rect))
-    assertEquals(200, area(rect))
+    assertEquals(rectPerimeter, perimeter(rect))
+    assertEquals(rectArea, area(rect))
   }
 
   @Test def testCircle(): Unit = {
     val circ:Shape = Circle(radius)
-    assertEquals(628, perimeter(circ))
-    assertEquals(31400, area(circ))
+    assertEquals(circPerimeter, perimeter(circ))
+    assertEquals(circArea, area(circ))
   }
 
   @Test def testSquare(): Unit = {
     val square:Shape = Square(edge)
-    assertEquals(200, perimeter(square))
-    assertEquals(2500, area(square))
+    assertEquals(squarePerimeter, perimeter(square))
+    assertEquals(squareArea, area(square))
   }
 
 }
