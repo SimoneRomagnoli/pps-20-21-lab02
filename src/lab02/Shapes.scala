9f8d62546ecac1a7bfabb4fbda2cc6cdbd055fc4
Simone Romagnoli <simone.romagnoli10@studio.unibo.it>
2021-03-08 17:16:15 +0100

    tested the perimeter of a rectangle

--- src/lab02/Shapes.scala
+++ src/lab02/Shapes.scala
@@ -2,10 +2,17 @@ package lab02
 
 object Shapes {
 
+  val pi =3.14
+
   sealed trait Shape
   case class Rectangle(height:Double, width:Double) extends Shape
   case class Circle(radius:Double) extends Shape
   case class Square(edge:Double) extends Shape
 
+  def perimeter(s:Shape): Double = s match {
+    case Rectangle(h, w) => h*2 + w*2
+    case Circle(r) => 2*pi*r
+    case Square(e) => 4*e
+  }
 
 }
--- test/lab02/TestShapes.scala
+++ test/lab02/TestShapes.scala
@@ -1,8 +1,16 @@
 package lab02
 
+import lab02.Shapes._
 import org.junit.jupiter.api.Assertions._
 import org.junit.jupiter.api.Test
 
 class TestShapes {
 
+  val height = 10.0
+  val width = 20.0
+
+  @Test def testRectangle(): Unit = {
+    val rect:Shape = Rectangle(height, width)
+    assertEquals(60, perimeter(rect))
+  }
 }
